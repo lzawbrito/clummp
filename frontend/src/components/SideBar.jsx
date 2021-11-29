d66a460739e6524d8388f856e348ee52a1e82fd7
Lucas Brito <luzabri7@gmail.com>
2021-11-29 16:22:24 -0500

    feat: info button, refactor: removed some files

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import Request from "./Request"
 import ControlPanel from './ControlPanel'
+import InfoIcon from '../icons/info-svgrepo-com.svg'
 
 export default function SideBar(props) {
 	function controlPanel(data) {
@@ -19,6 +20,11 @@ export default function SideBar(props) {
 
 	return <>
 	<div className="sideBar">
+		<div>
+			<a href='https://github.com/lzawbrito/clummp' target='_blank' rel="noreferrer">
+				<img id='info' src={InfoIcon} height='20px' alt="info"></img>
+			</a>
+		</div>
 		<div className="centerText">
 			<h1 id="title">CluMMP<span id='versionTag'>v0.1</span></h1> 
 			<div id="subtitle">(Cluster Merger Matching Program)</div>
